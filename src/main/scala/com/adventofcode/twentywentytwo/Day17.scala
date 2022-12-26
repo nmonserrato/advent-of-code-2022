30a470cf30cf6d3bfb9eb73c956d599fe39ef94e
nmonserrato <nmonserrato@users.noreply.github.com>
2022-12-26 18:33:41 +0100

    day17 part2

--- src/main/scala/com/adventofcode/twentywentytwo/Day17.scala
+++ src/main/scala/com/adventofcode/twentywentytwo/Day17.scala
@@ -1,34 +1,61 @@
 package com.adventofcode.twentywentytwo
 
+import scala.collection.mutable
 import scala.io.Source
 import scala.util.Using
 
 object Day17 {
-  val howManyRocks = 2022
   private val jetPattern: String = parseInput
+  private val howManyRocks = 1000000000000L // <-- change this to 2022 for part 1
   private val shapes = Array(Shapes.horizonalLine, Shapes.cross, Shapes.l, Shapes.verticalLine, Shapes.square)
+  private val tower = new Tower(jetPattern.size * 3)
+  private var nextJetIndex = 0
+  private var currentShape: Shape = shapes(0)
+  private val detectedShape0Positions = new mutable.HashMap[Int, (Long, Int)]()
+  private var simulatedHeight = -1L
 
   def main(args: Array[String]): Unit = {
-    val tower = new Tower()
-    var nextJetIndex = 0
-    var currentShape: Shape = null
-
-    for (nextShapeIndex <- 0 until howManyRocks) {
-      currentShape = shapes(nextShapeIndex % shapes.length)
-      var pos = tower.addNew(currentShape)
-      var movedDown = true
-
-      while (movedDown) {
-        if (jetPattern(nextJetIndex) == '>') pos = tower.moveRight(currentShape, pos)
-        else pos = tower.moveLeft(currentShape, pos)
-        val lastPos = pos
-        pos = tower.moveDown(currentShape, pos)
-        movedDown = pos != lastPos
-        nextJetIndex = (nextJetIndex + 1) % jetPattern.length
+    var nextShapeIndex = 0L
+    while (nextShapeIndex < howManyRocks) {
+      val shapeIndex = nextShapeIndex % shapes.length
+      if (shapeIndex == 0 && simulatedHeight < 0) {
+        val jetIndex = nextJetIndex%jetPattern.length
+        if (detectedShape0Positions.contains(jetIndex)) {
+          val heightPerIteration = tower.height - detectedShape0Positions(jetIndex)._2
+          val shapesPerIteration = nextShapeIndex - detectedShape0Positions(jetIndex)._1
+          val remainingShapes = howManyRocks - nextShapeIndex
+          val simulatedIterations = remainingShapes/shapesPerIteration
+          val simulatedShapes = simulatedIterations * shapesPerIteration
+          val extraShapes = (remainingShapes - simulatedIterations * shapesPerIteration).toInt
+          println(s"Each iteration adds $shapesPerIteration shapes and $heightPerIteration in height. " +
+            s"Remaining $remainingShapes shapes. Will simulate $simulatedShapes and play $extraShapes more.")
+          simulatedHeight = simulatedIterations * heightPerIteration
+          nextShapeIndex = howManyRocks - extraShapes
+        } else {
+          detectedShape0Positions(jetIndex) = (nextShapeIndex, tower.height)
+        }
       }
+      throwRock(nextShapeIndex)
+      nextShapeIndex += 1
     }
 
-    tower.print()
+    simulatedHeight = Math.max(simulatedHeight, 0)
+    println(simulatedHeight + tower.height)
+  }
+
+  private def throwRock(shapeIndex: Long): Unit = {
+    currentShape = shapes((shapeIndex % shapes.length).toInt)
+    var pos = tower.addNew(currentShape)
+    var movedDown = true
+
+    while (movedDown) {
+      if (jetPattern(nextJetIndex) == '>') pos = tower.moveRight(currentShape, pos)
+      else pos = tower.moveLeft(currentShape, pos)
+      val lastPos = pos
+      pos = tower.moveDown(currentShape, pos)
+      movedDown = pos != lastPos
+      nextJetIndex = (nextJetIndex + 1) % jetPattern.length
+    }
   }
 
   private def parseInput: String = {
@@ -40,12 +67,14 @@ object Day17 {
 
 private final case class PositionInTower(left: Int, bottom: Int)
 
-private class Tower {
+private class Tower(howManyShapesToFit: Int) {
   private val towerWidth = 7
-  private val data = new Array[Array[Boolean]](5 * Day17.howManyRocks + 5)
+  private val data = new Array[Array[Boolean]](5 * howManyShapesToFit + 5)
   private var highestRockY = 0
   data.indices.foreach(r => data(r) = Array.fill(towerWidth)(false))
   data(0) = Array.fill(towerWidth)(true)
+
+  def height = highestRockY
 
   def addNew(shape: Shape): PositionInTower = {
     val bl = PositionInTower(2, highestRockY + 4)
@@ -79,7 +108,7 @@ private class Tower {
   }
 
   def print(): Unit = {
-    ((highestRockY + 5)to highestRockY - 5 by -1).foreach { row =>
+    ((highestRockY + 5)to 0 by -1).foreach { row =>
       val pr = (0 until towerWidth).map(col => if (data(row)(col)) "#" else ".").mkString
       println(pr)
     }
